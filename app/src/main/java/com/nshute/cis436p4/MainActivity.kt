0803bb750868d297d3318ef5661a51f818f3b345
NickShute <nshute@umich.edu>
2022-04-24 17:11:06 -0400

    Initial design of UI added, along with test plan

--- app/src/main/java/com/nshute/cis436p4/MainActivity.kt
+++ app/src/main/java/com/nshute/cis436p4/MainActivity.kt
@@ -2,10 +2,19 @@ package com.nshute.cis436p4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val top: Fragment = Top()
+        val bottom: Fragment = Bottom()
+
+        val fm: FragmentManager = supportFragmentManager
+        fm.beginTransaction().replace(R.id.topFragment, top).commit()
+        fm.beginTransaction().replace(R.id.bottomFragment, bottom).commit()
     }
 }
